24c6576252db4a5de1da66e20ec8606cfc285e0a
Pdave <ferdinandpdave@gmail.com>
2024-12-17 00:25:54 -0400

    Made the base style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-bg": "hsl(230, 100%, 99%)",
+        "pricing-comp-bg": "hsl(0, 0%, 100%)",
+        "dark-desat-blue": "hsl(227, 35%, 25%)",
+        "grayish-blue": "hsl(225, 20%, 60%)",
+        "cust-discount": "hsl(15, 100%, 70%)",
+        "cust-discount-bg": "hsl(14, 92%, 95%)",
+        "cust-cta": "hsl(226, 100%, 87%)",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
